437cea524a551943b68b9d889050961d6d47b7d8
John Bywater <john.bywater@appropriatesoftware.net>
2021-12-04 15:37:48 +0000

    Cast when subclassing app class in system.

--- paxossystem/system.py
+++ paxossystem/system.py
@@ -1,5 +1,5 @@
 import itertools
-from typing import Type
+from typing import Type, cast
 
 from eventsourcing.system import System
 
@@ -11,13 +11,17 @@ class PaxosSystem(System):
         self.app_class = app_class
         self.num_participants = num_participants
         classes = [
-            type(
+            cast(Type[PaxosApplication], type(
                 f"{self.app_class.__name__}{i}",
                 (self.app_class,),
                 {"num_participants": self.num_participants},
-            )
+            ))
             for i in range(num_participants)
         ]
         assert num_participants > 1
         pipes = [[c[0], c[1], c[0]] for c in itertools.combinations(classes, 2)]
+        # pipes = [
+        #     [classes[0], classes[1], classes[0]],
+        #     [classes[0], classes[2], classes[0]],
+        # ]
         super(PaxosSystem, self).__init__(pipes)
